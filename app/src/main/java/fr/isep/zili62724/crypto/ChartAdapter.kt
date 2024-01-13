deba80226cf834e2e0effd091d836d947151b8ea
zihuaLi <13364064743@163.com>
2024-01-13 19:33:05 +0100

    change color and add search for chart

--- app/src/main/java/fr/isep/zili62724/crypto/ChartAdapter.kt
+++ app/src/main/java/fr/isep/zili62724/crypto/ChartAdapter.kt
@@ -1,15 +1,22 @@
 package fr.isep.zili62724.crypto
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import fr.isep.zili62724.crypto.databinding.ItemChartBinding
 
-class ChartAdapter(private val currencyList: List<CurrencyData>) : RecyclerView.Adapter<ChartAdapter.ChartViewHolder>() {
-
+class ChartAdapter(private var currencyList: List<CurrencyData>) : RecyclerView.Adapter<ChartAdapter.ChartViewHolder>() {
+    fun updateData(newList: List<CurrencyData>) {
+        currencyList = newList
+        notifyDataSetChanged()
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChartViewHolder {
         val binding = ItemChartBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ChartViewHolder(binding)
@@ -22,6 +29,11 @@ class ChartAdapter(private val currencyList: List<CurrencyData>) : RecyclerView.
 
     override fun getItemCount(): Int {
         return currencyList.size
+    }
+    fun setAnimation(view: View){
+        val anim= AlphaAnimation(0.0f,1.0f)
+        anim.duration=1000
+        view.startAnimation(anim)
     }
 
     class ChartViewHolder(private val binding: ItemChartBinding) :
@@ -40,6 +52,16 @@ class ChartAdapter(private val currencyList: List<CurrencyData>) : RecyclerView.
 
             val barData = BarData(dataSet)
             binding.barChart.data = barData
+            // 配置X轴
+            val xAxis = binding.barChart.xAxis
+            xAxis.valueFormatter = IndexAxisValueFormatter(arrayOf("1h", "24h", "7d"))
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            xAxis.granularity = 1f
+            xAxis.setDrawGridLines(false)
+            // 配置Y轴
+            binding.barChart.axisLeft.isEnabled = true
+            binding.barChart.axisRight.isEnabled = false
+
             binding.barChart.description.isEnabled = false
             binding.barChart.setFitBars(true)
             binding.barChart.invalidate() // refresh the chart
--- app/src/main/java/fr/isep/zili62724/crypto/ChartFragment.kt
+++ app/src/main/java/fr/isep/zili62724/crypto/ChartFragment.kt
@@ -1,5 +1,7 @@
 package fr.isep.zili62724.crypto
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,32 +11,54 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import fr.isep.zili62724.crypto.databinding.FragmentChartBinding
 import org.json.JSONObject
-import java.util.HashMap
 
 class ChartFragment : Fragment() {
     private var _binding: FragmentChartBinding? = null
     private val binding get() = _binding!!
+    private lateinit var chartAdapter: ChartAdapter
+    private var currencyList = listOf<CurrencyData>()
+    private var filteredList = listOf<CurrencyData>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentChartBinding.inflate(inflater, container, false)
         fetchData()
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSearchView()
+    }
+
+    private fun setupSearchView() {
+        binding.search2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                filterData(s.toString())
+            }
+        })
+    }
+
+    private fun filterData(searchQuery: String) {
+        filteredList = if (searchQuery.isEmpty()) {
+            currencyList
+        } else {
+            currencyList.filter {
+                it.name.contains(searchQuery, ignoreCase = true)
+            }
+        }
+        chartAdapter.updateData(filteredList)
+    }
+
     private fun fetchData() {
         val url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-        val queue = Volley.newRequestQueue(context)
+        val queue = Volley.newRequestQueue(requireContext())
         val jsonObjectRequest: JsonObjectRequest =
             object : JsonObjectRequest(Method.GET, url, null,
             Response.Listener { response ->
@@ -44,14 +68,15 @@ class ChartFragment : Fragment() {
             Response.ErrorListener { error ->
                 error.printStackTrace()
             }) {
-                override fun getHeaders(): Map<String, String> {
-                    val headers = HashMap<String, String>()
-                    headers["X-CMC_PRO_API_KEY"] = "3aefaa5b-0d4a-458b-b362-a4b2e1fa3d4b"
-                    return headers
-                }
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers["X-CMC_PRO_API_KEY"] = "3aefaa5b-0d4a-458b-b362-a4b2e1fa3d4b"
+                return headers
+            }
         }
         queue.add(jsonObjectRequest)
     }
+
     private fun parseData(response: JSONObject): List<CurrencyData> {
         val currencyList = ArrayList<CurrencyData>()
         val dataArray = response.getJSONArray("data")
@@ -68,14 +93,14 @@ class ChartFragment : Fragment() {
     }
 
     private fun setupRecyclerView(currencyList: List<CurrencyData>) {
-        val adapter = ChartAdapter(currencyList)
-        binding.recyclerView.layoutManager = LinearLayoutManager(context)
-        binding.recyclerView.adapter = adapter
+        chartAdapter = ChartAdapter(currencyList)
+        binding.RecyclerView2.layoutManager = LinearLayoutManager(context)
+        binding.RecyclerView2.adapter = chartAdapter
+        this.currencyList = currencyList
+        this.filteredList = currencyList
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
-
